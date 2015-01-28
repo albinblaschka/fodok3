5bd228e6f132a542d9cbddc71554788813039758
Albin Blaschka <albin.blaschka@standortsanalyse.net>
2015-01-28 18:08:04 +0100

    Abfragen über Web-API und Übertrag ins Formular begonnen (DOI); bei ISSN noch Problem mit Web-Adresse und Same-Origin

---
+++ js/addAuthorInputs.js
@@ -0,0 +1,11 @@
+fodok.addAuthorInputs = (function (){
+
+	fodok.authorCnt++;
+	var aship = fodok.addAuthorSnippet.replace(/°/g, fodok.authorCnt);
+	$('#authors').before(aship);
+	$('#aship_'+fodok.authorCnt).val('zweit');
+	$('#APK_'+fodok.authorCnt).change(function() {
+		fodok.insertNameByAPK($(this).val());
+	});
+
+});
