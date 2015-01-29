1c5ed93f5f4e5802f6c9d4f04ef72c82edb79bc7
Albin Blaschka <albin.blaschka@standortsanalyse.net>
2015-01-29 08:37:09 +0100

    Gehört noch zum vorherigen commit...

--- js/getMediaTypes.js
+++ js/getMediaTypes.js
@@ -39,24 +39,24 @@ fodok.getColumns = (function(which){
 			*/
 			$.each(json[which], function( index, value ) {
 				columns += '<div class="row">\
-								<div class="col-sm-2 text-right"><strong>' + json[which][index]['caption'] + '</strong></div>\
-								<div class="col-sm-10"><input class="form-control input-sm" name="' + index + '" type="text" placeholder="' + json[which][index]['caption'] + '"\
-									required="required" style="max-width:100%;" /></div>\
-							</div>';
+					<div class="col-sm-2 text-right"><strong>' + json[which][index]['caption'] + '</strong></div>\
+					<div class="col-sm-10"><input class="form-control input-sm" id="' + index + '" name="' + index + '" type="text" placeholder="' + json[which][index]['caption'] + '"\
+					required="required" style="max-width:100%;" /></div>\
+				</div>';
 				switch(index){
-					case 'DOI':
+					case 'doi':
 						$('#bttns').append('<button type="button" class="btn btn-success btn-sm btn-block" data-toggle="modal" data-target="#doiModal">Import via DOI</button>');
 						$('#fetchDOIData').click(function(){
 							var fetchDOI = fodok.fetchWebData('DOI');
 						});
 						break;
-					case 'ISSN':
+					case 'issn':
 						$('#bttns').append('<button type="button" class="btn btn-success btn-sm btn-block" data-toggle="modal" data-target="#issnModal">Import via ISSN</button>');
 						$('#fetchISSNData').click(function(){
 							var fetchISSN = fodok.fetchWebData('ISSN');
 						});
 						break;
-					case 'ISBN':
+					case 'isbn':
 						$('#bttns').append('<button type="button" class="btn btn-success btn-sm btn-block" data-toggle="modal" data-target="#isbnModal">Import via ISBN</button>');
 						$('#fetchISBNData').click(function(){
 							var fetchISBN = fodok.fetchWebData('ISBN');
@@ -78,15 +78,46 @@ fodok.getColumns = (function(which){
 });
 
 fodok.fetchWebData = (function(which){
+	
 	var DOIURL = 'http://api.crossref.org/works/';
+	var ISSNUrl = 'http://xissn.worldcat.org/webservices/xid/issn/||ISSN||?method=getHistory&format=json';
+	var ISBNUrl = 'http://xisbn.worldcat.org/webservices/xid/isbn/978-0-19-958067-5?method=getMetadata&format=json&fl=*';
+	
+	var convertISBNUrl = 'http://xisbn.worldcat.org/webservices/xid/isbn/978-3-902849-09-0?method=to10&format=json'
+	
 	switch(which){
 		case 'DOI':
 			// jQuery.get('http://api.crossref.org/works/10.1111/gfs.12063')
 			var theDOI = $('#DOIInput').val();
+			$('#doi').val(theDOI);
 			var request = DOIURL + theDOI;
 			var response = jQuery.get(request, function() {
-					var ref = response.responseJSON.message;
-				})
+				// Autoren auslesen, eintragen
+				// TODO: Prüfen, ob Vorname als Initial vorhanden?
+				var acnt = 1
+				var ref = response.responseJSON.message;
+				$.each(ref.author, function(index, value){
+					var addedInputs = fodok.addAuthorInputs();
+					$('#lastName_' + acnt).val(value.family);
+					$('#initials_' + acnt).val(value.given);
+					acnt++;
+				});
+				// Jahr
+				$('#year').val(ref['indexed']['date-parts'][0][0]);
+				// Titel der Arbeit
+				$('#titel').val(ref.title);
+				// Zeitschriften-Titel
+				$('#journal').val(ref['container-title'][0]);
+				// Volume
+				$('#volume').val(ref.volume);
+				// Nummer
+				$('#number').val(ref.issue);
+				// Seitenzahlen
+				var pages = ref.page;
+				var splitted = pages.split('-');
+				$('#pageFrom').val(splitted[0]);
+				$('#pageTo').val(splitted[1]);
+			})
 				.done(function() {
 					$('#doiModal').modal('hide');
 				})
@@ -100,8 +131,12 @@ fodok.fetchWebData = (function(which){
 
 			break;
 		case 'ISSN':
+			var theISSN = $('#ISSNInput').val();
+			var request = ISSNUrl.replace('||ISSN||', theISSN);
+			var response = jQuery.get(request, function(){
+			      console.log(repsonse);
+			});
 			break;
 		default:
 	}
-	console.log(which);
 });
